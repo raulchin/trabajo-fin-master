b95f74e78e94f3afd9c2ca27ef586abf8fb7fb09
ba0100062m <rchin@baustro.fin.ec>
2026-03-26 15:23:35 -0500

    Cambio de directorio del frontend y crear el micro gateway

---
+++ frontend/integrated-water-management-system/src/main.tsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <h1>Bienvenido a SIGAP</h1>
+  </StrictMode>,
+)
